f168d3e00e0308e07240b37978255b5fd5582003
Diki Andreano <92381721+panglima-jateng@users.noreply.github.com>
2023-01-18 11:13:00 +0700

    Add files via upload

---
+++ alquran.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+# author : panglima jateng
+# team   : aligator cyber indo
+# script : 1-30 juz alquran
+
+# database
+# https://github.com/prajurit-pajat/DkHjIpKl-_-_-vhakhsYBggOnavsjKihGfaoBfgabKhgs
+h=https;g=github;c=com;p=prajurit;t=pajat;d=DkHjIpKl;y="-_-_-";z=vhakhsYBggOnavsjKihGfaoBfgabKhgs;gc="git clone "
+panglima_jateng=echo;garis="==============================================="
+exploit(){
+author="panglima jateng";team="aligator cyber indo";script="1-30 juz alquran"
+}
+chos(){
+cd $d$y$z
+}
+awali="$gc$h";tengah="$awali://$g.$c";akhir="$tengah/$p-$t/$d$y$z"
+aligator_cyber_indo(){
+$akhir
+}
+kng='\e[0;33m';bru='\e[0;36m';abu='\e[1;30m';pth='\e[0m';hju='\e[1;32m'
+sleep 1
+echo -e $hju
+figlet installting
+termux-setup-storage
+sleep 3
+aligator_cyber_indo
+clear
+toilet -f standard -F metal al-quran
+exploit
+$panglima_jateng -e $hju$garis;$panglima_jateng -e $hju" {$bru•$hju}$pth author $bru :$pth "$author;$panglima_jateng -e $hju" {$bru•$hju}$pth team   $bru :$pth "$team;$panglima_jateng -e $hju" {$bru•$hju}$pth script $bru :$pth "$script;$panglima_jateng -e $hju$garis
+echo -e "  $hju         {$bru•$hju}$pth pilih juz 1-30$hju {$bru•$hju}"
+echo -e $bru"•$hju=============================================$bru•"
+echo -e $hju"|$bru>$kng juz $bru["$pth"01$bru]      $hju|$bru>$kng juz $bru["$pth"11$bru]      $hju|$bru>$kng juz $bru["$pth"21$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"02$bru]      $hju|$bru>$kng juz $bru["$pth"12$bru]      $hju|$bru>$kng juz $bru["$pth"22$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"03$bru]      $hju|$bru>$kng juz $bru["$pth"13$bru]      $hju|$bru>$kng juz $bru["$pth"23$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"04$bru]      $hju|$bru>$kng juz $bru["$pth"14$bru]      $hju|$bru>$kng juz $bru["$pth"24$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"05$bru]      $hju|$bru>$kng juz $bru["$pth"15$bru]      $hju|$bru>$kng juz $bru["$pth"25$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"06$bru]      $hju|$bru>$kng juz $bru["$pth"16$bru]      $hju|$bru>$kng juz $bru["$pth"26$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"07$bru]      $hju|$bru>$kng juz $bru["$pth"17$bru]      $hju|$bru>$kng juz $bru["$pth"27$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"08$bru]      $hju|$bru>$kng juz $bru["$pth"18$bru]      $hju|$bru>$kng juz $bru["$pth"28$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"09$bru]      $hju|$bru>$kng juz $bru["$pth"19$bru]      $hju|$bru>$kng juz $bru["$pth"29$bru]$hju |";echo -e $hju"|$bru>$kng juz $bru["$pth"10$bru]      $hju|$bru>$kng juz $bru["$pth"20$bru]      $hju|$bru>$kng juz $bru["$pth"30$bru]$hju |"
+echo -e $hju"•$hju=============================================$bru•"
+read -p "> " jus
+cd $d$y$z
+if [[ $jus == "1" ]] || [[ $jus == "01" ]]
+then
+    cp juz1.txt /storage/emulated/0/
+    echo "> juz 1 tersimpan di penyimpanan dengan format juz1.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "2" ]] || [[ $jus == "02" ]]
+then
+    cp juz2.txt /storage/emulated/0/
+    echo "> juz 2 tersimpan di penyimpanan dengan format juz2.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "3" ]] || [[ $jus == "02" ]]
+then
+    cp juz3.txt /storage/emulated/0/
+    echo "> juz 3 tersimpan di penyimpanan dengan format juz3.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "4" ]] || [[ $jus == "04" ]]
+then
+    cp juz4.txt /storage/emulated/0/
+    echo "> juz 4 tersimpan di penyimpanan dengan format juz4.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "5" ]] || [[ $jus == "05" ]]
+then
+    cp juz5.txt /storage/emulated/0/
+    echo "> juz 5 tersimpan di penyimpanan dengan format juz5.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "6" ]] || [[ $jus == "06" ]]
+then
+    cp juz6.txt /storage/emulated/0/
+    echo "> juz 6 tersimpan di penyimpanan dengan format juz6.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "7" ]] || [[ $jus == "07" ]]
+then
+    cp juz7.txt /storage/emulated/0/
+    echo "> juz 7 tersimpan di penyimpanan dengan format juz7.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "8" ]] || [[ $jus == "08" ]]
+then
+    cp juz8.txt /storage/emulated/0/
+    echo "> juz 8 tersimpan di penyimpanan dengan format juz8.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "9" ]] || [[ $jus == "09" ]]
+then
+    cp juz9.txt /storage/emulated/0/
+    echo "> juz 9 tersimpan di penyimpanan dengan format juz9.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "10" ]]; then
+    cp juz10.txt /storage/emulated/0/
+    echo "> juz 10 tersimpan di penyimpanan dengan format juz10.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "11" ]]; then
+    cp juz11.txt /storage/emulated/0/
+    echo "> juz 11 tersimpan di penyimpanan dengan format juz11.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "12" ]]; then
+    cp juz12.txt /storage/emulated/0/
+    echo "> juz 12 tersimpan di penyimpanan dengan format juz12.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "13" ]]; then
+    cp juz13.txt /storage/emulated/0/
+    echo "> juz 13 tersimpan di penyimpanan dengan format juz13.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "14" ]]; then
+    cp juz14.txt /storage/emulated/0/
+    echo "> juz 14 tersimpan di penyimpanan dengan format juz14.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "15" ]]; then
+    cp juz15.txt /storage/emulated/0/
+    echo "> juz 15 tersimpan di penyimpanan dengan format juz15.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "16" ]]; then
+    cp juz16.txt /storage/emulated/0/
+    echo "> juz 16 tersimpan di penyimpanan dengan format juz16.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "17" ]]; then
+    cp juz17.txt /storage/emulated/0/
+    echo "> juz 17 tersimpan di penyimpanan dengan format juz17.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "18" ]]; then
+    cp juz18.txt /storage/emulated/0/
+    echo "> juz 18 tersimpan di penyimpanan dengan format juz18.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "19" ]]; then
+    cp juz19.txt /storage/emulated/0/
+    echo "> juz 19 tersimpan di penyimpanan dengan format juz19.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "20" ]]; then
+    cp juz20.txt /storage/emulated/0/
+    echo "> juz 20 tersimpan di penyimpanan dengan format jut20.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "21" ]]; then
+    cp juz21.txt /storage/emulated/0/
+    echo "> juz 21 tersimpan di penyimpanan dengan format juz21.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "22" ]]; then
+    cp juz22.txt /storage/emulated/0/
+    echo "> juz 22 tersimpan di penyimpanan dengan format juz22.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "23" ]]; then
+    cp juz23.txt /storage/emulated/0/
+    echo "> juz 23 teraimpan di penyimpanan dengan format juz23.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "24" ]]; then
+    cp juz24.txt /storage/emulated/0/
+    echo "> juz 24 tersimpan di penyimpanan dengan format juz24.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "25" ]]; then
+    cp juz25.txt /storage/emulated/0/
+    echo "> juz 25 tersimpan di penyimpanan dengan format juz25.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "26" ]]; then
+    cp juz26.txt /storage/emulated/0/
+    echo "> juz 26 tersimpan di penyimpanan dengan format juz26.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "27" ]]; then
+    cp juz27.txt /storage/emulated/0/
+    echo "> juz 27 tersimpan di penyimpanan dengan format juz27.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "28" ]]; then
+    cp juz28.txt /storage/emulated/0/
+    echo "> juz 28 tersimpan di penyimpanan dengan format juz28.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "29" ]]; then
+    cp juz29.txt /storage/emulated/0/
+    echo "> juz 29 tersimpan di penyimpanan dengan format juz29.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+elif [[ $jus == "30" ]]; then
+    cp juz30.txt /storage/emulated/0/
+    echo "> juz 30 tersimpan di penyimpanan dengan format juz30.txt"
+    echo "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+fi
